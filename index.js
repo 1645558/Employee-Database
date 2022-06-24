ca05b53263f0eadd7810856a07f1a818442462eb
Rare Raptor <connorwtuncc@gmail.com>
2022-06-23 22:16:51 -0400

    required class files in index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const inquirer = require('inquirer');
+const Department = require('./lib/department');
+const Role = require('./lib/role');
+const Employee = require('./lib/employee');
 
 inquirer.prompt ([
     {
