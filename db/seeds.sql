ce21f2d044742b149ae6d62eccf2ccf58b0f9e56
Rare Raptor <connorwtuncc@gmail.com>
2022-06-27 16:54:37 -0400

    changed some info in seeds file and ran again to see if there was a difference

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,9 +3,9 @@ VALUES ('IT'),
 ('Sales');
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('Engineer', 120.000, 12),
-('Manager', 80.000, 13),
-('Sales Lead', 100.000, 14);
+VALUES ('Engineer', 120.000, 1),
+('Manager', 80.000, 2),
+('Sales Lead', 100.000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('George', 'Hendy', 33, 44);
