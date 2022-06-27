9ff059b0e66fb1eb016f3d02d474a1f3e7ad1786
Rare Raptor <connorwtuncc@gmail.com>
2022-06-27 16:37:03 -0400

    trying different setups to fix field id value error

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,8 +4,8 @@ VALUES ('IT'),
 
 INSERT INTO role (title, salary, department_id)
 VALUES ('Engineer', 120.000, 12),
-('Manager', 80.000, 13)
-('Sales Lead', 100.000, 14)
+('Manager', 80.000, 13),
+('Sales Lead', 100.000, 14);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('George', 'Hendy', 33, 44);
